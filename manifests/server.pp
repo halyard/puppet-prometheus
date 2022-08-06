c5d7cc410a5d2b38ab648d702527f96e956ea2fc
Les Aker <me@lesaker.org>
2022-08-06 19:57:52 -0400

    now with more nginx

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,10 +1,33 @@
 # @summary Install Prometheus server
 #
-class prometheus::server {
+# @param hostname is the hostname for metrics reading
+# @param tls_account is the account details for requesting the TLS cert
+# @param grafana_password is the password for grafana to access metrics
+# @param tls_challengealias is the domain to use for TLS cert validation
+class prometheus::server (
+  String $hostname,
+  String $tls_account,
+  String $grafana_password,
+  Optional[String] $tls_challengealias = undef,
+) {
   package { 'prometheus': }
 
-  -> service { 'prometheus':
+  -> file { '/etc/conf.d/prometheus':
+    ensure => file,
+    source => 'puppet:///modules/prometheus/conf',
+  }
+
+  ~> service { 'prometheus':
     ensure => running,
     enable => true,
+  }
+
+  nginx::site { $hostname:
+    proxy_target       => 'http://localhost:9090',
+    tls_challengealias => $tls_challengealias,
+    tls_account        => $tls_account,
+    users              => {
+      'grafana'  => $grafana_password,
+    },
   }
 }
