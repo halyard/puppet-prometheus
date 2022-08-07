344170f2cb40c5e2e646854e839a84ed9c64d859
Les Aker <me@lesaker.org>
2022-08-06 21:20:05 -0400

    fix syntax

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@
 # @param server_ip is the IP address of the prometheus server
 # @param address is how this server is reached
 class prometheus (
-  String $server_ip
+  String $server_ip,
   String $address = $facts['networking']['ip'],
 ) {
   file { '/etc/identifier':
